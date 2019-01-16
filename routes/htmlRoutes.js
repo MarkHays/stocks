4d5e390d149357a167e9c3c367a96e5c4e035f9b
MarkHays <mark.hays1991@gmail.com>
2019-01-15 21:04:16 -0800

    adding chart.html and bootstrap

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,12 +3,12 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
+    // db.Positions.findAll({}).then(function(dbExamples) {
+      res.render("index");
         //msg: "Welcome to Stock X",
-        examples: dbExamples
-      });
-    });
+  //       examples: dbExamples
+  //     });
+  //   });
   });
 
   // Load example page and pass in an example by id
