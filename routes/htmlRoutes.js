35f3fe90e128b7f87b19a469dc3bfa94ba74f4e6
KitKat <katherine_nguyen7@yahoo.com>
2019-01-17 11:06:51 -0800

    put new user data into an object

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -16,6 +16,10 @@ module.exports = function(app) {
     res.render("createUser");
   });
 
+  app.post("/new-user", function(req, res) {
+    console.log(req.body);
+  });
+
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExamples) {
